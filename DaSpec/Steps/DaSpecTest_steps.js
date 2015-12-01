88c28c449022a1213aadc889cea8772bfd2c284b
Gojko Adzic <gojko@gojko.com>
2015-12-01 22:50:28 +0100

    configs loading using driver

--- DaSpec/Steps/DaSpecTest_steps.js
+++ DaSpec/Steps/DaSpecTest_steps.js
@@ -3,13 +3,9 @@
  */
 
 defineStep(/Navigate to "(.*)"/, function (page) {
-  console.log(page);
   return browser.get(page).then(function () {
     browser.getTitle().then(function (title) {
-      title.should.equal(page);
-      console.log('title ' + title);
+	  expect(page).toEqual(title);
     });
-    'Test Assertion'.should.equal('Test Assertion');
-    console.log('visited ' + page);
   });
 });
--- DaSpec/config.js
+++ DaSpec/config.js
@@ -1,23 +1,17 @@
-/**
- * Created by PhilBeadle on 10/09/15.
- */
-var configHelper = require('./configHelper.js');
-
+/*global exports*/
 exports.config = {
-  onPrepare: function () {
-    configHelper.setUpConfigGlobals();
-  },
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  "specs": ["Specs/*.md"],
-  "daSpecOptions": {
-    "specs": ["DaSpec/Specs/*.md"],
-    "steps": ["DaSpec/Steps/**/*.js"],
-    "output-dir": "daspec-output"
-  },
-  baseUrl: 'http://localhost:8081/',
-
-  framework: 'custom',
-  frameworkPath: '../node_modules/protractor/lib/frameworks/daspec.js'
+	capabilities: {
+		'browserName': 'chrome'
+	},
+	'specs': ['Specs/*.md'],
+	daSpecOptions: {
+		'specs': ['DaSpec/Specs/*.md'],
+		'steps': ['DaSpec/Steps/**/*.js'],
+		'formatters': ['daspec/formatters/console', 'daspec/formatters/markdown-files'],
+		'output-dir': 'daspec-output',
+		encoding: 'utf8'
+	},
+	baseUrl: 'http://localhost:8081/',
+	framework: 'custom',
+	frameworkPath: '../node_modules/daspec-js-protractor-driver/lib/daspec-protractor-driver.js'
 };
